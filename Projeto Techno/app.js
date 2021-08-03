f3eb3896f1f2c408faecc535c6f8a4b27ad113c7
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-08-03 20:54:14 -0300

    Projeto Techno - Fetch Produto

--- Projeto Techno/app.js
+++ Projeto Techno/app.js
@@ -2,6 +2,7 @@ const vm = new Vue({
   el: '#app',
   data: {
     produtos: [],
+    produto: false,
   },
   filters: {
     numeroPreco(valor) {
@@ -15,7 +16,11 @@ const vm = new Vue({
     async fetchProdutos() {
       const data = await fetch('./api/produtos.json');
       this.produtos = await data.json();
-    }
+    },
+    async fetchProduto(id) {
+      const data = await fetch(`./api/produtos/${id}/dados.json`);
+      this.produto = await data.json();
+    },
   },
   created() {
     this.fetchProdutos();
