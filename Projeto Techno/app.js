d7deeba37836d4d940abb72ef9573b800a20a603
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-08-03 20:20:55 -0300

    Projeto Techno - Filters

--- Projeto Techno/app.js
+++ Projeto Techno/app.js
@@ -3,6 +3,14 @@ const vm = new Vue({
   data: {
     produtos: [],
   },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", {
+        style: 'currency',
+        currency: 'BRL'
+      });
+    },
+  },
   methods: {
     async fetchProdutos() {
       const data = await fetch('./api/produtos.json');
