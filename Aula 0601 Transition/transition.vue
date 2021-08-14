1c11618bb12c27577682720b7391e33909c82fa1
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-08-14 10:18:03 -0300

    Aula 0601 Transition

---
+++ Aula 0601 Transition/transition.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button @click="ativo = !ativo">Ativar</button>
+    <transition 
+      appear
+      name='fade'
+      @enter='enter'
+      @leave='leave'
+    >
+      <h1>Serviços</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ativo: false,
+    }
+  },
+  methods: {
+    enter(elemento) {
+      console.log(elemento);
+    },
+    leave() {
+      console.log('Elemento sumiu.');
+    },
+  }
+}
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 3s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
