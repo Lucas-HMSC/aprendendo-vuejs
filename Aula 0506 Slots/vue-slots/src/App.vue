d420c63d77cef00c2c0bd8d64bf209feae922140
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-08-13 21:15:45 -0300

    Aula 0506 Slots

---
+++ Aula 0506 Slots/vue-slots/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <ModalPrincipal>
+      <template v-slot:header="{logo}">
+        Esse é o header
+        {{ logo }}
+      </template>
+      <template v-slot:default="{user}">
+        {{ user.name }}
+        <p>Teste</p>
+        <button>Comprar</button>
+      </template>
+      <template v-slot:footer>
+        <p>Esse é o footer</p>
+      </template>
+    </ModalPrincipal>
+  </div>
+</template>
+
+<script>
+import ModalPrincipal from './components/ModalPrincipal.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ModalPrincipal,
+  },
+}
+</script>
+
+<style>
+
+</style>
