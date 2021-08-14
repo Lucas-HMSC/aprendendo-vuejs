522cfca54646a271076f929e600cf120ac822b94
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-08-14 12:04:33 -0300

    Aula 0602 Components

---
+++ Aula 0602 Components/home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul>
+      <li @click='componenteAtivo = "PaginaSobre"'>Sobre</li>
+      <li @click='componenteAtivo = "PaginaServicos"'>Serviços</li>
+    </ul>
+    <transition mode='out-in'>
+      <component :is='componenteAtivo'></component>
+    </transition>
+  </div>
+</template>
+
+<script>
+import PaginaSobre from './PaginaSobre.vue';
+import PaginaServicos from './PaginasServicos.vue';
+
+export default {
+  data() {
+    return {
+      componenteAtivo: 'PaginaSobre',
+    }
+  },
+  components: {
+    PaginaSobre,
+    PaginaServicos,
+  },
+}
+</script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: all .3s;
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+  transform: translate3d(0, -20px, 0);
+}
+</style>
