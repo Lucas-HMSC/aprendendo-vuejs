f50423836b702a0ddd473a512fc8055356369f58
Lucas Carvalho <lucashms.carvalho@gmail.com>
2021-08-19 17:18:37 -0300

    Aula 0708 Projeto Header

--- Projeto Techno-Cursos/techno/src/router.js
+++ Projeto Techno-Cursos/techno/src/router.js
@@ -1,11 +1,30 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
 import Home from './views/Home.vue';
+import Cursos from './views/Cursos.vue';
+import Contato from './views/Contato.vue';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/contato',
+      name: 'contato',
+      component: Contato
+    },
+    {
+      path: '/cursos',
+      name: 'cursos',
+      component: Cursos
+    }
+  ],
 })
