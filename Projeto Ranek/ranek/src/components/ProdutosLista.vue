e70c3f7f8adcab483f30ab1de8569b2f68d15afe
Lucas Carvalho <lucashms.carvalho@gmail.com>
2021-08-20 15:41:11 -0300

    Projeto Ranek - Produtos

--- Projeto Ranek/ranek/src/components/ProdutosLista.vue
+++ Projeto Ranek/ranek/src/components/ProdutosLista.vue
@@ -1,10 +1,34 @@
 <template>
-  <div></div>
+  <section class="produtos-container">
+    <div v-for='produto in produtos' :key='produto.id'>
+      <img 
+        v-if='produto.fotos'
+        :src='produto.fotos[0].src' 
+        :alt='produto.fotos[0].titulo'
+      >
+      <p class="preco">{{ produto.preco }}</p>
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p>{{ produto.descricao }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      produtos: null,
+    }
+  },
+  methods: {
+    async getProdutos() {
+      const data = await fetch('http://localhost:3000/produto');
+      this.produtos = await data.json();
+    },
+  },
+  created() {
+    this.getProdutos();
+  },
 }
 </script>
 
