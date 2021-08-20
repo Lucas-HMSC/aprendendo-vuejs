6d290f953f3065b42a11f3faf1d88febfd8264de
Lucas Carvalho <lucashms.carvalho@gmail.com>
2021-08-20 11:05:31 -0300

    Aula 0801 State

---
+++ Aula 0800 - Vuex/app-vuex/src/components/Aula.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>Aula {{ totalAulas }}, usuario {{ nomeMaiusculo }}, {{ aulasCompletas }}</p>
+    <button @click='mostrarConsole'>
+      Mostrar no Console
+    </button>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  data() {
+    return {
+      totalAulas: 25,
+      nome: 'Lucas',
+    }
+  },
+  computed: {
+    ...mapState(['user', 'aulasCompletas']),
+    nomeMaiusculo() {
+      return this.nome.ToUpperCase();
+    },
+  },
+  methods: {
+    mostrarConsole() {
+      console.log(this.user);
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
