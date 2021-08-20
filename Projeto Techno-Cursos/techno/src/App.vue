57888e45343a7ee1b4ad5cb3d9c712832829be54
Lucas Carvalho <lucashms.carvalho@gmail.com>
2021-08-20 09:43:14 -0300

    Aula 0711 Home

--- Projeto Techno-Cursos/techno/src/App.vue
+++ Projeto Techno-Cursos/techno/src/App.vue
@@ -38,9 +38,34 @@ a.router-link-active {
   color: #4b8;
 }
 
+img {
+  max-width: 100%;
+}
+
 .container {
   max-width: 800px;
   margin: 0 auto;
   padding: 20px;
+}
+
+.v-enter {
+  opacity: 0;
+  transform: translate3d(-29px, 0, 0);
+}
+
+.v-enter-active {
+  transition: all .3s;
+}
+
+.conteudo {
+  display: grid;
+  grid-template-columns: 1fr minmax(200px, 400px);
+  grid-gap: 30px;
+}
+
+@media screen and (max-width: 400px) {
+  .conteudo {
+    display: block;
+  }
 }
 </style>
