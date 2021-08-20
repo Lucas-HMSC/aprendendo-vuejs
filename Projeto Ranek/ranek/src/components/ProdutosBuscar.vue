53d84507b1f69eee11c2e3d6584a8879df26e6bf
Lucas Carvalho <lucashms.carvalho@gmail.com>
2021-08-20 16:16:34 -0300

    Projeto Ranek - Busca

--- Projeto Ranek/ranek/src/components/ProdutosBuscar.vue
+++ Projeto Ranek/ranek/src/components/ProdutosBuscar.vue
@@ -1,10 +1,36 @@
 <template>
-  <div></div>
+  <form>
+    <input 
+      name="busca"
+      id="busca"
+      type="text"
+      v-model='busca'
+    >
+    <input 
+      id="lupa"
+      type="submit"
+      value="Buscar"
+      @click.prevent='buscarProdutos'
+    >
+  </form>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      busca: '',
+    }
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({
+        query: {
+          q: this.busca
+        }
+      });
+    },
+  },
 }
 </script>
 
