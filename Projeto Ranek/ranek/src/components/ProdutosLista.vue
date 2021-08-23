a078062357350a409e1f9d17686014c477039c12
Lucas Carvalho <lucashms.carvalho@gmail.com>
2021-08-23 09:53:06 -0300

    Projeto Ranek - Produto Filter

--- Projeto Ranek/ranek/src/components/ProdutosLista.vue
+++ Projeto Ranek/ranek/src/components/ProdutosLista.vue
@@ -11,13 +11,13 @@
           v-for='(produto, index) in produtos'
           :key='index'
         >
-          <RouterLink to='/'>
+          <RouterLink :to='{ name: "Produto", params: {id: produto.id} }'>
             <img
               v-if='produto.fotos'
               :src='produto.fotos[0].src'
               :alt='produto.fotos[0].titulo'
             >
-            <p class="preco">{{ produto.preco }}</p>
+            <p class="preco">{{ produto.preco | numeroPreco }}</p>
             <h2 class="titulo">{{ produto.nome }}</h2>
             <p>{{ produto.descricao }}</p>
           </RouterLink>
