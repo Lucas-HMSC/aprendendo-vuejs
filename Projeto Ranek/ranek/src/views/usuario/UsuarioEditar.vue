b2ea021ac2244abf49b1d07e1833f49b596537c1
Lucas Carvalho <lucashms.carvalho@gmail.com>
2021-08-24 11:59:21 -0300

    Projeto Ranek - Usuário Atualizar

--- Projeto Ranek/ranek/src/views/usuario/UsuarioEditar.vue
+++ Projeto Ranek/ranek/src/views/usuario/UsuarioEditar.vue
@@ -1,10 +1,40 @@
 <template>
-  <p>Usuário Editar</p>
+  <section>
+    <UsuarioForm>
+      <button 
+        class="btn"
+        @click.prevent='atualizarUsuario'
+      > 
+        Atualizar Usuário
+      </button>
+
+    </UsuarioForm>
+  </section>
 </template>
 
 <script>
-export default {
+import { api } from '@/services.js';
 
+import UsuarioForm from '@/components/UsuarioForm.vue';
+
+export default {
+  name: 'UsuarioEditar',
+  components: {
+    UsuarioForm,
+  },
+  methods: {
+    atualizarUsuario() {
+      api
+        .put(`/usuario/${this.$store.state.usuario.id}`, this.$store.state.usuario)
+        .then(() => {
+          this.$store.dispatch('getUsuario', this.$store.state.usuario.id);
+          this.$router.push({ name: 'Usuario' });
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
+  },
 }
 </script>
 
